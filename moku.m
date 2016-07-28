cae0e0e92fe7be4af378f5eb1499f8af74477785
Ben Nizette <benjamin.nizette@anu.edu.au>
2016-07-28 11:43:12 +1000

    Add a second control function that accepts kwargs rather than positional args

--- moku.m
+++ moku.m
@@ -28,7 +28,6 @@ classdef moku
             rpcstruct.params = varargin;
             jsonstruct = savejson('', rpcstruct);
             nid = nid + 1;
-
             opts = weboptions('MediaType','application/json', 'Timeout', 10);
             jsonresp = webwrite(['http://' obj.IP '/rpc/call'], jsonstruct, opts);
             resp = loadjson(jsonresp);
@@ -38,6 +37,41 @@ classdef moku
                     resp.error.message);
             end
             status = resp.result;
+        end
+
+        function status = mokuctl2(obj, action, varargin)
+            x = varargin;
+            if isempty(x)
+                args = struct;
+            elseif iscell(x)
+                % Cells can be {name,val;...} or {name,val,...}
+                if (size(x,1) == 1) && size(x,2) > 2
+                    % Reshape {name,val, name,val, ... } list to {name,val; ... }
+                    x = reshape(x, [2 numel(x)/2])';
+                end
+
+                if size(x,2) ~= 2
+                    error('Invalid args: cells must be n-by-2 {name,val;...} or vector {name,val,...} list');
+                end
+
+                % Convert {name,val, name,val, ...} list to struct
+                if ~iscellstr(x(:,1))
+                    error('Invalid names in name/val argument list');
+                end
+                % Little trick for building structs from name/vals
+                % This protects cellstr arguments from expanding into nonscalar structs
+                x(:,2) = num2cell(x(:,2));
+                x = x';
+                x = x(:);
+                args = struct(x{:});
+            elseif isstruct(x)
+                if ~isscalar(x)
+                    error('struct args must be scalar');
+                end
+                args = x;
+            end
+
+            status = mokuctl(obj, action, args);
         end
 
         function Frame = get.Frame(obj)
